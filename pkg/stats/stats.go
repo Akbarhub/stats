9b4730ab2483e1fb395adf499bcff3eb318cd878
babaevakbars@gmail.com <Akbarjon@go.alif.academy>
2023-02-06 18:59:29 +0500

    Добавлена функция PeriodsDynamic

--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -19,3 +19,24 @@ func CategoriesAvg(payments []types.Payment) map[types.Category]types.Money{
  	return categories
 }
 
+// PeriodsDynamic сравнивает расходы по категориям за два периода.
+func PeriodsDynamic(
+	first map[types.Category]types.Money, second map[types.Category]types.Money,
+)map[types.Category]types.Money{
+	prDinamic := map[types.Category]types.Money{}
+	for key, value := range second {
+		_, status := first[key]
+		if	!status{
+			prDinamic[key] = value
+		}
+		prDinamic[key] = value - first[key]
+	}
+	for key := range first{
+		_, status := second[key]
+		if	!status {
+			prDinamic[key] = 0
+		}
+	}
+	
+	return prDinamic
+}
